f995e84b9fe331afb6f251766697677c1a09ecc4
ArgonautAli <haider.salar@gmail.com>
2024-10-15 23:38:32 +0530

    Feat: dash ALMOST DONE

--- Dash.gd
+++ Dash.gd
@@ -1,10 +1,25 @@
 extends Node2D
 
 @onready var timer: Timer = $Timer
+@onready var cooldown_timer: Timer = $CooldownTimer
+
+var cooldown_duration: float = 0.02  # Set the cooldown duration here
+var is_on_cooldown: bool = false
 
 func start_dash(duration: float) -> void:
-	timer.wait_time = duration
-	timer.start()
+	if not is_on_cooldown:  # Check if the dash is on cooldown
+		timer.wait_time = duration
+		timer.start()
+		start_cooldown()  # Start the cooldown
+
+func start_cooldown() -> void:
+	is_on_cooldown = true
+	cooldown_timer.start()  # Start the cooldown timer
+
 
 func is_dashing() -> bool:
 	return !timer.is_stopped()
+
+
+func _on_cooldown_timer_timeout() -> void:
+	is_on_cooldown = false 
--- scripts/player_2.gd
+++ scripts/player_2.gd
@@ -1,70 +1,57 @@
 extends CharacterBody2D
-@onready var dash: Node2D = $Dash
 
+@onready var dash: Node2D = $Dash
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 const JUMP_VELOCITY = -450.0
-const DASH_SPEED = 2000
-const DASH_DURATION = 0.002
+const DASH_SPEED = 1500
+const DASH_DURATION = 0.2  # Adjust duration as needed
 const MOVE_SPEED = 200
 
 var is_jumped = false
-var speed = 200.0
+var speed = MOVE_SPEED
 
 func _physics_process(delta: float) -> void:
-	
 	if Input.is_action_just_pressed("dash"):
 		dash.start_dash(DASH_DURATION)
-		#velocity.x = 1 * DASH_SPEED
-		
-	speed = DASH_SPEED if dash.is_dashing() else MOVE_SPEED
-		
 
+	speed = DASH_SPEED if dash.is_dashing() else MOVE_SPEED
 
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
-	#if Input.is_action_just_pressed("dash"):
-		#dash.start_dash(DASH_DURATION)
-		
-	# Double jump 
+
+	# Handle double jump
 	if Input.is_action_just_pressed("jump") and is_jumped:
 		is_jumped = false
 		velocity.y = JUMP_VELOCITY
 
-	
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		is_jumped = true
 		velocity.y = JUMP_VELOCITY
-		
+
 	var direction := Input.get_axis("move_left", "move_right")
-	
-	#handle block
-		
+
+	# Handle block
 	if Input.is_action_pressed("block") and is_on_floor():
 		direction = 0
 		animated_sprite.play("block")
 
-	# Get the input direction and handle the movement/deceleration.
-	
-#	# play animation
+	# Play animation based on direction
 	if direction == 0 and not Input.is_action_pressed("block"):
 		animated_sprite.play("idle")
-		
 	elif not Input.is_action_pressed("block"):
 		animated_sprite.play("run")
-	
-	
+
+	# Flip character based on direction
 	if direction > 0:
 		animated_sprite.flip_h = false
 	elif direction < 0:
 		animated_sprite.flip_h = true 
+
 	if not is_on_floor(): 
 		animated_sprite.play("jump")
-		
 
-	
 	if direction:
 		velocity.x = direction * speed
 	else:
